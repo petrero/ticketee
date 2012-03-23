2340366b74e80af0ef731cb21a1d25353cce56db
Petre Diaconescu <petrero@gmail.com>
2012-03-23 23:15:59 +0200

    Add validation to ensure names are specified when creating projects

--- spec/requests/creating_projects_spec.rb
+++ spec/requests/creating_projects_spec.rb
@@ -11,5 +11,14 @@ describe "Projects" do
       page.should have_content("Project has been created.") 
       page.should have_content("TextMate 2 - Projects - Ticketee") 
     end
+    
+    it "can't create a project with invalid attributes" do
+      visit('/')
+      click_link("New Project") 
+      fill_in("Name", :with => "")
+      click_on("Create Project") 
+      page.should have_content("Project has not been created.")
+      page.should have_content("Name can't be blank") 
+    end
   end
 end
