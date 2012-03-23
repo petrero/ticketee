d8a306d2ae33c40775cd9f85d3a83f4b395eabc0
Petre Diaconescu <petrero@gmail.com>
2012-03-23 23:04:42 +0200

    Added title functionality for show page

--- spec/requests/creating_projects_spec.rb
+++ spec/requests/creating_projects_spec.rb
@@ -7,7 +7,9 @@ describe "Projects" do
       click_link("New Project")
       fill_in("Name", :with => "TextMate 2")
       click_on("Create Project")
-      page.should have_content("Project has been created.")  
+      current_path.should == project_path(Project.find_by_name!("TextMate 2"))
+      page.should have_content("Project has been created.") 
+      page.should have_content("TextMate 2 - Projects - Ticketee") 
     end
   end
 end
