9a601fcc5bd2aec0de3dc822e39252ddc1b2a7df
Petre Diaconescu <petrero@gmail.com>
2012-03-24 19:55:32 +0200

    Implemented edit action for the tickets controller

---
+++ spec/requests/editing_tickets_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe "EditingTickets" do
+  let(:project){Factory(:project)}
+  let(:ticket){Factory(:ticket, :project => project)}
+  
+  before do
+    @projects = Array.new()
+    @projects.push(project)
+    @tickets = Array.new()
+    @tickets.push(ticket)
+    visit('/')
+    click_link(project.name)
+    click_link(ticket.title)
+    click_link("Edit Ticket")
+  end
+  
+  
+  it "update ticket with valid attributes" do
+    fill_in("Title", :with => "TextMate 2")
+    click_on("Update Ticket")
+    page.should have_content("Ticket has been updated.")
+    page.should have_css("#ticket h2", :text => "TextMate 2")
+    page.should have_no_content(ticket.title)
+  end
+  
+  it "don't update ticket with invalid attributes" do
+    fill_in("Title", :with => "") 
+    click_on("Update Ticket") 
+    page.should have_content("Ticket has not been updated.")
+  end
+end
