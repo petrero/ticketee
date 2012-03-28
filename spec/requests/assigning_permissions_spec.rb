f2fcd6116e6d3cb15c6aa12e59a066a4c5039a4f
Petre Diaconescu <petrero@gmail.com>
2012-03-28 20:00:39 +0300

    Added creating, editing, updating and deleting tickets to assigning permissions interface

--- spec/requests/assigning_permissions_spec.rb
+++ spec/requests/assigning_permissions_spec.rb
@@ -4,6 +4,7 @@ describe "AssigningPermissions" do
   let(:admin){Factory(:admin_user)}
   let(:user){Factory(:confirmed_user)}
   let(:project){Factory(:project)}
+  let(:ticket){Factory(:ticket, :user => user, :project => project)}
   
   before do
     @users = Array.new()
@@ -11,6 +12,8 @@ describe "AssigningPermissions" do
     @users.push(user) 
     @projects = Array.new()
     @projects.push(project)
+    @tickets = Array.new()
+    @tickets.push(ticket)
     login_as(admin)
     visit('/')
     click_link("Admin")
@@ -22,10 +25,83 @@ describe "AssigningPermissions" do
   it "viewing a project" do
     page.check("permissions_#{project.id}_#{:view.to_sym}")
     click_button("Update")
-    logout()
+    logout()    
     
     login_as(user)
+    visit('/')
     page.should have_content(project.name)  
   end
   
+  it "creating tickets for a project" do
+    page.check("permissions_#{project.id}_view")
+    page.check("permissions_#{project.id}_create_tickets") 
+    click_button("Update")
+    logout()
+    
+    login_as(user)
+    visit('/')
+    click_link(project.name)
+    page.should have_css('a', :text => "New Ticket")  
+  end
+  
+  it "can't create tickets for a project" do
+    page.check("permissions_#{project.id}_view")
+    click_button("Update")
+    logout()
+    
+    login_as(user)
+    visit('/')
+    click_link(project.name)
+    page.should have_no_css('a', :text => "New Ticket")  
+  end
+  
+  it "updating a ticket for a project" do
+    page.check("permissions_#{project.id}_view")
+    page.check("permissions_#{project.id}_edit_tickets") 
+    click_button("Update")
+    logout()  
+    
+    login_as(user)
+    visit('/')
+    click_link(project.name)
+    click_link(ticket.title)
+    page.should have_css('a', :text => "Edit Ticket")
+  end
+  
+  it "can't update a ticket for a project" do
+    page.check("permissions_#{project.id}_view")
+    click_button("Update")
+    logout()  
+    
+    login_as(user)
+    visit('/')
+    click_link(project.name)
+    click_link(ticket.title)
+    page.should have_no_css('a', :text => "Edit Ticket")
+  end
+  
+  it "deleting a ticket for a project" do
+    page.check("permissions_#{project.id}_view")
+    page.check("permissions_#{project.id}_delete_tickets") 
+    click_button("Update")
+    logout()  
+    
+    login_as(user)
+    visit('/')
+    click_link(project.name)
+    click_link(ticket.title)
+    page.should have_css('a', :text => "Delete Ticket")
+  end
+  
+  it "can't delete a ticket for a project" do
+    page.check("permissions_#{project.id}_view")
+    click_button("Update")
+    logout()  
+    
+    login_as(user)
+    visit('/')
+    click_link(project.name)
+    click_link(ticket.title)
+    page.should have_no_css('a', :text => "Delete Ticket")
+  end
 end
