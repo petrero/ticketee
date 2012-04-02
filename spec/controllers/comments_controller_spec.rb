d6e466655246f78083a32396ad09f80f5d415815
Petre Diaconescu <petrero@gmail.com>
2012-04-02 17:59:49 +0300

    Protect state_id from users who do not have permission to change it

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe CommentsController do
-
+  let(:user){Factory(:confirmed_user)}
+  let(:project){Factory(:project)}
+  let(:ticket){Factory(:ticket, :user => user, :project => project)}
+  
+  let(:state){State.create!(:name => "New")}
+  
+  before do
+    sign_in(:user, user)
+  end
+  
+  it "cannot transition a state by passing through state id" do
+    post :create, {:comment => {:text => "Hacked!", :state_id => state.id}, :ticket_id => ticket.id}
+    
+    ticket.reload
+    ticket.state.should eql(nil)
+  end
 end
