ffa341a5bfb718a40f5cdbb00f9488680aeec757
Petre Diaconescu <petrero@gmail.com>
2012-04-03 18:55:17 +0300

    Users may now search for tickets by state or tag

--- spec/requests/searchings_spec.rb
+++ spec/requests/searchings_spec.rb
@@ -3,8 +3,13 @@ require 'spec_helper'
 describe "Searchings" do
   let(:user){Factory(:confirmed_user)}
   let(:project){Factory(:project)}
-  let(:ticket1){Factory(:ticket, :title => "Ticket tag", :project => project, :user => user)}
-  let(:ticket2){Factory(:ticket, :title => "Ticket tagged", :project => project, :user => user)}
+  let(:ticket1_state) {State.create(:name => "ticket1_state")}
+  let(:ticket2_state) {State.create(:name => "ticket2_state")}
+  let(:ticket1){Factory(:ticket, :title => "Ticket tag", :project => project, :user => user, :state_id => ticket1_state.id)}
+  let(:ticket2){Factory(:ticket, :title => "Ticket tagged", :project => project, :user => user, :state_id => ticket2_state.id)}
+  let(:ticket1_state) {State.create(:name => "ticket1_state")}
+  let(:ticket2_state) {State.create(:name => "ticket2_state")}
+  
   
   before do
     Factory(:permission, :user => user, :thing => project, :action => "view")
@@ -13,15 +18,20 @@ describe "Searchings" do
     ticket2.tags = [Tag.create(:name => "ticket2_tag")]
     login_as(user)
     visit('/')
+    click_link(project.name)
   end
   
-  it "finding by tag" do
-    click_link(project.name)
+  it "finding by tag" do   
     fill_in("Search", :with => "tag:ticket1_tag")
     click_button("Search")
     page.should have_content(ticket1.title)
     page.should have_no_content(ticket2.title)
   end
   
-  
+  it "finding by state" do
+    fill_in("Search", :with => "state:ticket1_state")
+    click_button("Search")
+    page.should have_content(ticket1.title)
+    page.should have_no_content(ticket2.title)  
+  end
 end
